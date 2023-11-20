4c5deb4a67b9724bd5412ce4dad2d1ff19ec4c69
Khalil Melhem <khalil.i.melhem@gmail.com>
2023-11-20 19:28:25 +0300

    chore: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next";
+
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Yu Rooms",
+  description:
+    "Need a quiet room at Al Yamamah University? Look no further! With Yu Rooms, you can take advantage of your free time in a space of your choice.",
+};
 
 export default function RootLayout({
   children,
