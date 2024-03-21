46d7d5e9bf6cd647dca0c5f0d780aed68ee6039c
Mohammed Jebreen <msj4413007@gmail.com>
2024-03-22 02:54:08 +0300

    Main page + Building for phone and desktop

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,10 +1,10 @@
 import bgImage1 from "./bgImage1.svg";
 import bgImage2 from "./bgImage2.svg";
 import bgImage3 from "./bgImage3.svg";
-
+import Gdscyu from "./Navbar.svg";
 import yuLogo from "./yu-logo.svg";
 
 import mainBulding from "./main-building.png";
 import tuwaiq from "./tuwaiq.png";
 
-export { bgImage1, bgImage2, bgImage3, yuLogo, mainBulding, tuwaiq };
+export { bgImage1, bgImage2, bgImage3, yuLogo, mainBulding, tuwaiq, Gdscyu };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,42 @@
-import GoogleButton from "@/components/temp/google_button";
-import { GoogleColor, Buildings } from "@/constants/constants";
-import { bgImage1 } from "../../assets/images";
 import Image from "next/image";
+import Link from "next/link";
+import { bgImage2, Gdscyu } from "../../assets/images"; 
+
 export default function Home() {
   return (
-    <main className="flex h-[100lvh] flex-col items-center justify-center p-5 text-center md:gap-y-10">
+    <div className="relative min-h-screen flex flex-col">
       <Image
-        src={bgImage1}
-        alt="background-image"
-        className="absolute h-full w-full object-cover"
-      ></Image>
-    </main>
+        src={bgImage2}
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="z-0"
+      />
+
+
+      <Link href="https://gdscyu.netlify.app/">
+        <div className="w-64 h-64 md:w-64 md:h-64 absolute top-8 left-8 md:left-14 cursor-pointer z-30">
+          <Image src={Gdscyu} alt="Yu Rooms Logo" layout="fill" objectFit="contain" />
+        </div>
+      </Link>
+
+      <div className="flex-grow flex items-center justify-center p-8 z-10">
+
+        <div className="bg-white md:bg-transparent rounded-t-3xl md:rounded-none shadow-lg md:shadow-none text-center w-full md:w-auto p-6 absolute bottom-0 md:relative md:p-0 z-20">
+          <h1 className="text-4xl md:text-6xl text-orange-500 font-bold mb-4 md:mb-0">
+            Navigate Your Learning Spaces with Ease
+          </h1>
+          <p className="text-xl md:text-2xl text-gray-800 md:text-white mt-4 mb-6 md:mt-0">
+            Your academic journey starts here.
+          </p>
+          <Link href="/building">
+            <div className="inline-block px-8 py-4 bg-orange-500 text-white text-xl font-semibold rounded-full hover:bg-orange-600 transition-colors cursor-pointer">
+              Check Availability!
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
